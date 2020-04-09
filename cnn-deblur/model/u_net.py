55c4c0795ec558235411a0de1f91c46a3a7e8d5d
nihil <mattia.orlandi@studio.unibo.it>
2020-04-09 18:15:28 +0200

    Added KL Divergence and BinaryAccuracy

--- cnn-deblur/model/u_net.py
+++ cnn-deblur/model/u_net.py
@@ -5,7 +5,7 @@ from tensorflow.keras.layers import Input
 from tensorflow.keras.optimizers import Adam
 from utils.loss_functions import *
 from tensorflow.keras.losses import BinaryCrossentropy, MeanSquaredError, KLDivergence
-from tensorflow.keras.metrics import binary_accuracy
+from tensorflow.keras.metrics import BinaryAccuracy
 from typing import Tuple, Optional
 
 
@@ -68,4 +68,4 @@ class UNet(ConvNet):
             'mse': MeanSquaredError(),
             'kld': KLDivergence()
         })
-        self.model.compile(Adam(learning_rate=1e-4), loss=loss_dict[loss], metrics=[binary_accuracy])
+        self.model.compile(Adam(learning_rate=1e-4), loss=loss_dict[loss], metrics=[BinaryAccuracy])
