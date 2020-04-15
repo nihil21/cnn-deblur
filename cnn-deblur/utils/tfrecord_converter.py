0e234372a9269a113a6be5a4d77e571a195eae48
nihil <mattia.orlandi@studio.unibo.it>
2020-04-15 19:10:44 +0200

    Added utilities for managing TFRecords

--- cnn-deblur/utils/tfrecord_converter.py
+++ cnn-deblur/utils/tfrecord_converter.py
@@ -25,13 +25,13 @@ def reds_to_tfrecords(out_dir, blur_base, sharp_base):
         }))
 
     # Iterate over blur and sharp subdirectories
-    for blur_sub, sharp_sub in tqdm(zip(blur_subdirs, sharp_subdirs)):
+    for blur_sub, sharp_sub in tqdm(zip(blur_subdirs, sharp_subdirs), total=len(blur_subdirs)):
         sub_name = os.path.basename(os.path.normpath(blur_sub))
         blur_batch = glob(os.path.join(blur_sub, '*.png'))
         sharp_batch = glob(os.path.join(sharp_sub, '*.png'))
 
         # For each subdirectory, create a .tfrecords file where Examples objects are stored
         with tf.io.TFRecordWriter(os.path.join(out_dir, 'REDS{0}.tfrecords'.format(sub_name))) as writer:
-            for blur_img, sharp_img in tqdm(zip(blur_batch, sharp_batch)):
+            for blur_img, sharp_img in tqdm(zip(blur_batch, sharp_batch), total=len(blur_batch)):
                 example = _convert_to_example(blur_img, sharp_img)
                 writer.write(example.SerializeToString())
