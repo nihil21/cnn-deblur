842a86e8b8b13c9dd0fd58c46b046a47e5fc3093
nihil <mattia.orlandi@studio.unibo.it>
2020-04-15 19:06:56 +0200

    Added utilities for managing TFRecords

--- cnn-deblur/utils/tfrecord_converter.py
+++ cnn-deblur/utils/tfrecord_converter.py
@@ -3,40 +3,35 @@ from glob import glob
 import os
 from tqdm import tqdm
 
-BASE_DIR = '/run/media/nihil/Backup/REDS/val'
-BLUR_BASE = '/run/media/nihil/Backup/REDS/val/val_blur'
-SHARP_BASE = '/run/media/nihil/Backup/REDS/val/val_sharp'
 
+def reds_to_tfrecords(out_dir, blur_base, sharp_base):
+    # Get lists of blur and sharp subdirectories
+    blur_subdirs = glob(os.path.join(blur_base, '*', ''))
+    sharp_subdirs = glob(os.path.join(sharp_base, '*', ''))
 
-def _bytes_feature(value):
-    """Returns a bytes_list from a string / byte."""
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+    # Define function to convert a string into a list of bytes
+    def _bytes_feature(value):
+        """Returns a bytes_list from a string / byte."""
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
+    # Define a function to convert the pair of blurred and sharp images into a Tensorflow Example object
+    def _convert_to_example(blur_path, sharp_path):
+        blur_buf = open(blur_path, 'rb').read()
+        sharp_buf = open(sharp_path, 'rb').read()
 
-def _convert_to_example(blur_path, sharp_path):
-    blur_buf = open(blur_path, 'rb').read()
-    sharp_buf = open(sharp_path, 'rb').read()
+        return tf.train.Example(features=tf.train.Features(feature={
+            'blur': _bytes_feature(blur_buf),
+            'sharp': _bytes_feature(sharp_buf)
+        }))
 
-    return tf.train.Example(features=tf.train.Features(feature={
-        'blur': _bytes_feature(blur_buf),
-        'sharp': _bytes_feature(sharp_buf)
-    }))
-
-
-def main():
-    blur_subdirs = glob(os.path.join(BLUR_BASE, '*', ''))
-    sharp_subdirs = glob(os.path.join(SHARP_BASE, '*', ''))
-
+    # Iterate over blur and sharp subdirectories
     for blur_sub, sharp_sub in tqdm(zip(blur_subdirs, sharp_subdirs)):
         sub_name = os.path.basename(os.path.normpath(blur_sub))
         blur_batch = glob(os.path.join(blur_sub, '*.png'))
         sharp_batch = glob(os.path.join(sharp_sub, '*.png'))
 
-        with tf.io.TFRecordWriter(os.path.join(BASE_DIR, 'REDS{0}.tfrecords'.format(sub_name))) as writer:
+        # For each subdirectory, create a .tfrecords file where Examples objects are stored
+        with tf.io.TFRecordWriter(os.path.join(out_dir, 'REDS{0}.tfrecords'.format(sub_name))) as writer:
             for blur_img, sharp_img in tqdm(zip(blur_batch, sharp_batch)):
                 example = _convert_to_example(blur_img, sharp_img)
                 writer.write(example.SerializeToString())
-
-
-if __name__ == '__main__':
-    main()
