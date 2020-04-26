7490bb27343a2b967853ba4822fba8a02655ae90
GiacomoPinardi <pinobo1997@gmail.com>
2020-04-26 23:36:46 +0200

    test baseline performance improvements

--- cnn-deblur/test_ssim_baseline.py
+++ cnn-deblur/test_ssim_baseline.py
@@ -2,23 +2,26 @@ import tensorflow as tf
 import numpy as np
 import glob
 from datasets import cifar_dataset
+import math
 
 from model.unet20 import UNet20
-from model.u_net import UNet
+from model.res_skip_unet import ResSkipUNet
 from datasets.reds_dataset import  load_image_dataset
 
 def ssim(trueY, predY):
     return tf.image.ssim(trueY, predY, max_val=1.)
 
+def psnr(trueY, predY):
+    return tf.image.psnr(trueY, predY, max_val=1.)
+
 # reds
 """
 _, _, test = load_image_dataset("/home/uni/dataset/", 21000, 3000, [288, 512], 4, 10, 42)
-test = test.batch(1)
+test = test.batch(4)
 
 weights = glob.glob('/home/uni/weights/reds/ep:020-val_loss:0.538.hdf5')
 
 conv_net = UNet20(input_shape=(288, 512, 3))
-conv_net.compile(loss='mae')
 conv_net.model.load_weights(weights[0])
 """
 
@@ -32,29 +35,31 @@ testX = tf.data.Dataset.from_tensor_slices(testX)
 testY = tf.data.Dataset.from_tensor_slices(testY)
 
 test = tf.data.Dataset.zip((testX, testY))
-test = test.batch(1)
-print(test.take(1))
-weights = glob.glob('/home/uni/weights/cifar/ep:050-val_loss:-31.109.hdf5')
+test = test.batch(512)
+weights = glob.glob('/content/drive/My Drive/cnn-deblur/resskipunet/ep:049-val_loss:-30.774.hdf5')
 
-conv_net = UNet(input_shape=(32, 32, 3))
-conv_net.compile(loss='mae')
+conv_net = ResSkipUNet(input_shape=(32, 32, 3))
 conv_net.model.load_weights(weights[0])
 
 ssims = []
+psnrs = []
 
-i = 0
 for pair in test:
-    i += 1
-    print(i)
-
     x = pair[0]
     y = pair[1]
 
     predicted = tf.convert_to_tensor(conv_net.predict(x))
 
-    delta = ssim(predicted, y) - ssim(x, y)
-    ssims.append(delta)
+    delta_s = ssim(predicted, y) - ssim(x, y)
+    delta_p = psnr(predicted, y) - psnr(x, y)
 
-avg = sum(ssims) / len(ssims)
+    ssims += delta_s.numpy().tolist()
+    psnrs += delta_p.numpy().tolist()
 
-print("Average improvement: {}".format(avg))
+avg_ssim = sum(ssims) / len(ssims)
+# remove -inf elements
+psnrs_clean = [x for x in psnrs if x != -math.inf]
+avg_psnr = sum(psnrs_clean) / len(psnrs_clean)
+
+print("Average improvement SSIM: {}".format(avg_ssim))
+print("Average improvement PSNR: {}".format(avg_psnr))
