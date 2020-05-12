c2611f2abf066f8a2d2fe50194bd95ba34be8785
nihil <mattia.orlandi@studio.unibo.it>
2020-05-13 01:29:40 +0200

    Added psnr_metric to custom_losses_metrics.py

--- cnn-deblur/model/custom_losses_metrics.py
+++ cnn-deblur/model/custom_losses_metrics.py
@@ -22,9 +22,9 @@ def content_loss(trueY, predY):
     return 0.5 * K.sum(K.square(trueY - predY))
 
 
-def psnr_loss(trueY, predY):
-    return -psnr_metric(trueY, predY)
-
-
 def psnr_metric(trueY, predY):
     return tf.image.psnr(trueY, predY, max_val=1.)
+
+
+def psnr_loss(trueY, predY):
+    return -psnr_metric(trueY, predY)
