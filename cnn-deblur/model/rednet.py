69b02d3ddbeba2169aa910b2bca1ffab74c7405f
nihil <mattia.orlandi@studio.unibo.it>
2020-05-17 17:19:25 +0200

    Added norm constraints to rednet.py

--- cnn-deblur/model/rednet.py
+++ cnn-deblur/model/rednet.py
@@ -1,6 +1,7 @@
 from model.conv_net import ConvNet
 from tensorflow.keras.layers import Input, Layer, Conv2D, Conv2DTranspose, Add
 from tensorflow.keras.models import Model
+from tensorflow.keras.constraints import min_max_norm
 from typing import Tuple, List, Optional
 
 
@@ -17,6 +18,7 @@ def encode(in_layer: Layer, num_layers: Optional[int] = 15, num_filters: Optiona
                    strides=stride,
                    padding='same',
                    activation='relu',
+                   kernel_constraint=min_max_norm(min_value=0., max_value=1.),
                    name='encode{0:d}'.format(i))(x)
         layers.append(x)
     return layers
@@ -31,10 +33,16 @@ def decode(res_layers: List[Layer], num_layers: Optional[int] = 15, num_filters:
                             strides=1,
                             padding='same',
                             activation='relu',
+                            kernel_constraint=min_max_norm(min_value=0., max_value=1.),
                             name='decode{0:d}'.format(i))(x)
         if i % 2 != 0:
             x = Add()([x, res_layers[i]])
-    x = Conv2DTranspose(filters=3, kernel_size=3, strides=2, padding='same', name='output')(x)
+    x = Conv2DTranspose(filters=3,
+                        kernel_size=3,
+                        strides=2,
+                        padding='same',
+                        activation='sigmoid',
+                        name='output')(x)
     x = Add()([x, res_layers[-1]])
     return x
 
