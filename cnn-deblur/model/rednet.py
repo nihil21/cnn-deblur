137dab5c52a29182cc6c2da2509ce9646fbc4198
nihil <mattia.orlandi@studio.unibo.it>
2020-05-18 21:49:18 +0200

    Modified structure of network

--- cnn-deblur/model/rednet.py
+++ cnn-deblur/model/rednet.py
@@ -6,16 +6,16 @@ from typing import Tuple, List, Optional
 
 
 def encode(in_layer: Layer, num_layers: Optional[int] = 15, num_filters: Optional[int] = 64) -> List[Layer]:
-    layers: List[Layer] = [in_layer]
+    layers: List[Layer] = []
     x = in_layer
     for i in range(num_layers):
-        if i == 0:
+        """if i == 0:
             stride = 2
         else:
-            stride = 1
+            stride = 1"""
         x = Conv2D(filters=num_filters,
                    kernel_size=3,
-                   strides=stride,
+                   strides=1,
                    padding='same',
                    activation='relu',
                    kernel_constraint=min_max_norm(min_value=0., max_value=1.),
@@ -28,7 +28,7 @@ def decode(res_layers: List[Layer], num_layers: Optional[int] = 15, num_filters:
     res_layers.reverse()
     x = res_layers[0]
     for i in range(num_layers):
-        x = Conv2DTranspose(filters=num_filters,
+        x = Conv2DTranspose(filters=3 if i == num_layers - 1 else num_filters,
                             kernel_size=3,
                             strides=1,
                             padding='same',
@@ -37,14 +37,14 @@ def decode(res_layers: List[Layer], num_layers: Optional[int] = 15, num_filters:
         if i % 2 != 0:
             x = Add()([x, res_layers[i]])
         x = ReLU()(x)
-    x = Conv2DTranspose(filters=3,
+    """x = Conv2DTranspose(filters=3,
                         kernel_size=3,
                         strides=2,
                         padding='same',
                         kernel_constraint=min_max_norm(min_value=0., max_value=1.),
                         name='output')(x)
     x = Add()([x, res_layers[-1]])
-    x = ReLU()(x)
+    x = ReLU()(x)"""
 
     return x
 
