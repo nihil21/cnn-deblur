7a7417ac8f6611eedd416adf2266cbce0760e0db
nihil <mattia.orlandi@studio.unibo.it>
2020-08-01 19:20:21 +0200

    Access batch_size using tf.data.Dataset property

--- cnn-deblur/models/deblur_gan.py
+++ cnn-deblur/models/deblur_gan.py
@@ -109,10 +109,10 @@ class DeblurGan(Model):
 
     @tf.function
     def train_step(self,
-                   batch: Tuple[tf.Tensor, tf.Tensor],
-                   batch_size: int):
+                   batch: Tuple[tf.Tensor, tf.Tensor]):
         blurred_batch = batch[0]
         sharp_batch = batch[1]
+        batch_size = blurred_batch._batch_size.numpy()
 
         d_losses = []
         # Train the critic multiple times according to critic_updates (by default, 5)
@@ -157,9 +157,8 @@ class DeblurGan(Model):
     @tf.function
     def distributed_train_step(self,
                                batch: tf.Tensor,
-                               batch_size: int,
                                strategy: Optional[tf.distribute.Strategy] = None):
-        per_replica_results = strategy.run(self.train_step, args=(batch, batch_size,))
+        per_replica_results = strategy.run(self.train_step, args=(batch,))
         reduced_d_loss = strategy.reduce(tf.distribute.ReduceOp.MEAN,
                                          per_replica_results['d_loss'], axis=None)
         reduced_g_loss = strategy.reduce(tf.distribute.ReduceOp.MEAN,
@@ -186,7 +185,7 @@ class DeblurGan(Model):
             psnr_metrics = []
             for batch in notebook.tqdm(train_data, total=steps_per_epoch):
                 # Perform train step
-                step_result = self.train_step(batch, batch_size)
+                step_result = self.train_step(batch)
 
                 # Collect results
                 d_losses.append(step_result['d_loss'])
@@ -201,7 +200,6 @@ class DeblurGan(Model):
     def distributed_training(self,
                              train_data: tf.Tensor,
                              epochs: int,
-                             batch_size: int,
                              steps_per_epoch: int,
                              strategy: tf.distribute.Strategy):
         for ep in notebook.tqdm(range(epochs)):
@@ -212,7 +210,7 @@ class DeblurGan(Model):
             psnr_metrics = []
             for batch in notebook.tqdm(train_data, total=steps_per_epoch):
                 # Perform train step
-                step_result = self.distributed_train_step(batch, batch_size, strategy)
+                step_result = self.distributed_train_step(batch, strategy)
 
                 # Collect results
                 d_losses.append(step_result['d_loss'])
