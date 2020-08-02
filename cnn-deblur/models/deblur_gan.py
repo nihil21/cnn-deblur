4e49f71585aa3581e1e554d4f285235753468315
nihil <mattia.orlandi@studio.unibo.it>
2020-08-02 23:02:20 +0200

    Updated Generator and Discriminator (Critic) architectures

--- cnn-deblur/models/deblur_gan.py
+++ cnn-deblur/models/deblur_gan.py
@@ -1,50 +1,141 @@
-from models.rednet import REDNet10
 from utils.custom_metrics import psnr, ssim
 import numpy as np
 import tensorflow as tf
+import tensorflow_addons as tfa
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, ELU, Flatten, Dense
+from tensorflow.keras.layers import Layer, Input, Conv2D, Conv2DTranspose, Dropout, Add, Activation, ELU
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications import VGG16
 from utils.custom_losses import wasserstein_loss, perceptual_loss
 from tqdm import notebook
-from typing import Tuple, List, Optional, Union
+from typing import Tuple, Optional, Union
 
 
 # Function to build generator and critic
+def res_block(in_layer: Layer,
+              layer_id: int,
+              filters: Optional[int] = 256,
+              kernel_size: Optional[Tuple[int]] = 3,
+              use_dropout: Optional[bool] = False):
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               padding='same',
+               name='res_block_conv{:d}_1'.format(layer_id))(in_layer)
+    x = tfa.layers.InstanceNormalization(name='res_block_in{:d}_1'.format(layer_id))(x)
+    x = ELU(name='res_block_elu{:d}'.format(layer_id))(x)
+    if use_dropout:
+        x = Dropout(rate=0.5,
+                    name='res_block_drop{:d}'.format(layer_id))(x)
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               padding='same',
+               name='res_block_conv{:d}_2'.format(layer_id))(x)
+    x = tfa.layers.InstanceNormalization(name='res_block_in{:d}_2'.format(layer_id))(x)
+    x = Add(name='res_block_add{:d}'.format(layer_id))([x, in_layer])
+    return x
+
+
 def create_generator(input_shape):
-    generator = REDNet10(input_shape=input_shape).model
-    generator._name = "Generator"
+    in_layer = Input(input_shape)
+    # Block 1
+    x = Conv2D(filters=64,
+               kernel_size=7,
+               padding='same',
+               name='conv1')(in_layer)
+    x = tfa.layers.InstanceNormalization(name='in1')(x)
+    x = ELU(name='elu1')(x)
+    # Block 2
+    x = Conv2D(filters=128,
+               kernel_size=3,
+               strides=2,
+               padding='same',
+               name='conv2')(x)
+    x = tfa.layers.InstanceNormalization(name='in2')(x)
+    x = ELU(name='elu2')(x)
+    # Block 3
+    x = Conv2D(filters=256,
+               kernel_size=3,
+               strides=2,
+               padding='same',
+               name='conv3')(x)
+    x = tfa.layers.InstanceNormalization(name='in3')(x)
+    x = ELU(name='elu3')(x)
+    # ResBlocks
+    for i in range(9):
+        x = res_block(in_layer=x,
+                      layer_id=i,
+                      use_dropout=True)
+    # Block 4
+    x = Conv2DTranspose(filters=128,
+                        kernel_size=3,
+                        strides=2,
+                        padding='same',
+                        name='conv4')(x)
+    x = tfa.layers.InstanceNormalization(name='in4')(x)
+    x = ELU(name='elu4')(x)
+    # Block 5
+    x = Conv2DTranspose(filters=64,
+                        kernel_size=3,
+                        strides=2,
+                        padding='same',
+                        name='conv5')(x)
+    x = tfa.layers.InstanceNormalization(name='in5')(x)
+    x = ELU(name='elu5')(x)
+    # Block 6
+    x = Conv2D(filters=3,
+               kernel_size=7,
+               padding='same',
+               activation='tanh',
+               name='conv6')(x)
+    out_layer = Add(name='add')([x, in_layer])
+
+    generator = Model(inputs=in_layer, outputs=out_layer, name='Generator')
     return generator
 
 
-def create_critic(input_shape,
-                  filters: List[int],
-                  kernels: List[int]):
-    visible = Input(input_shape)
+def create_critic(input_shape):
+    in_layer = Input(input_shape)
+    # Block 1
+    x = Conv2D(filters=64,
+               kernel_size=4,
+               strides=2,
+               padding='same',
+               name='conv1')(in_layer)
+    x = tfa.layers.InstanceNormalization(name='in1')(x)
+    x = ELU(name='elu1')(x)
+    # Block 2
+    x = Conv2D(filters=128,
+               kernel_size=4,
+               strides=2,
+               padding='same',
+               name='conv2')(x)
+    x = tfa.layers.InstanceNormalization(name='in2')(x)
+    x = ELU(name='elu2')(x)
+    # Block 3
+    x = Conv2D(filters=256,
+               kernel_size=4,
+               strides=2,
+               padding='same',
+               name='conv3')(x)
+    x = tfa.layers.InstanceNormalization(name='in3')(x)
+    x = ELU(name='elu3')(x)
+    # Block 4
+    x = Conv2D(filters=512,
+               kernel_size=4,
+               strides=1,
+               padding='same',
+               name='conv4')(x)
+    x = tfa.layers.InstanceNormalization(name='in4')(x)
+    x = ELU(name='elu4')(x)
+    # Block 5
+    x = Conv2D(filters=1,
+               kernel_size=4,
+               strides=1,
+               padding='same',
+               name='conv5')(x)
+    out_layer = Activation('sigmoid')(x)
 
-    x = visible
-    for i in range(len(filters)):
-        if i == 0:
-            x = Conv2D(filters=filters[i],
-                       kernel_size=kernels[i],
-                       strides=2,
-                       padding='same',
-                       name='conv{:d}'.format(i))(x)
-            x = BatchNormalization(name='bn{:d}'.format(i))(x)
-        else:
-            x = Conv2D(filters=filters[i],
-                       kernel_size=kernels[i],
-                       strides=1,
-                       padding='same',
-                       name='conv{:d}'.format(i))(x)
-        x = ELU(name='act{:d}'.format(i))(x)
-
-    x = Flatten(name='flat')(x)
-    x = Dense(1024, activation='tanh', name='dense')(x)
-    output = Dense(1, activation='sigmoid', name='output')(x)
-
-    return Model(inputs=visible, outputs=output, name='Discriminator')
+    return Model(inputs=in_layer, outputs=out_layer, name='Discriminator')
 
 
 class DeblurGan(Model):
@@ -54,9 +145,7 @@ class DeblurGan(Model):
         # Build generator
         self.generator = create_generator(input_shape)
         # Build critic (discriminator)
-        self.critic = create_critic(input_shape,
-                                    filters=[64, 128, 256],
-                                    kernels=[7, 3, 3])
+        self.critic = create_critic(input_shape)
 
         # Set loss_model, based on VGG16, to compute perceptual loss
         vgg = VGG16(include_top=False, weights='imagenet', input_shape=(None, None, 3))
@@ -151,8 +240,8 @@ class DeblurGan(Model):
 
         return {"d_loss": tf.reduce_mean(d_losses),
                 "g_loss": g_loss,
-                "ssim": ssim_metric,
-                "psnr": psnr_metric}
+                "ssim": tf.reduce_mean(ssim_metric),
+                "psnr": tf.reduce_mean(psnr_metric)}
 
     @tf.function
     def distributed_train_step(self,
@@ -181,6 +270,30 @@ class DeblurGan(Model):
         elif isinstance(train_data, Tuple):
             self.__train_on_tensor(train_data, epochs, steps_per_epoch)
 
+    def __train_on_dataset(self,
+                           train_data: tf.data.Dataset,
+                           epochs: int,
+                           steps_per_epoch: int):
+        for ep in notebook.tqdm(range(epochs)):
+            # Set up lists that will contain losses and metrics for each epoch
+            d_losses = []
+            g_losses = []
+            ssim_metrics = []
+            psnr_metrics = []
+            for batch in notebook.tqdm(train_data, total=steps_per_epoch):
+                # Perform train step
+                step_result = self.train_step(batch)
+
+                # Collect results
+                d_losses.append(step_result['d_loss'])
+                g_losses.append(step_result['g_loss'])
+                ssim_metrics.append(step_result['ssim'])
+                psnr_metrics.append(step_result['psnr'])
+
+            # Display results
+            print('Ep: {:d} - d_loss: {:f} - g_loss: {:f} - ssim: {:f} - psnr: {:f}\n'
+                  .format(ep, np.mean(d_losses), np.mean(g_losses), np.mean(ssim_metrics), np.mean(psnr_metrics)))
+
     def __train_on_tensor(self,
                           train_data: Tuple[np.ndarray, np.ndarray],
                           epochs: int,
@@ -203,30 +316,6 @@ class DeblurGan(Model):
 
                 # Perform train step
                 step_result = self.train_step((blurred_batch, sharp_batch))
-
-                # Collect results
-                d_losses.append(step_result['d_loss'])
-                g_losses.append(step_result['g_loss'])
-                ssim_metrics.append(step_result['ssim'])
-                psnr_metrics.append(step_result['psnr'])
-
-            # Display results
-            print('Ep: {:d} - d_loss: {:f} - g_loss: {:f} - ssim: {:f} - psnr: {:f}\n'
-                  .format(ep, np.mean(d_losses), np.mean(g_losses), np.mean(ssim_metrics), np.mean(psnr_metrics)))
-
-    def __train_on_dataset(self,
-                           train_data: tf.data.Dataset,
-                           epochs: int,
-                           steps_per_epoch: int):
-        for ep in notebook.tqdm(range(epochs)):
-            # Set up lists that will contain losses and metrics for each epoch
-            d_losses = []
-            g_losses = []
-            ssim_metrics = []
-            psnr_metrics = []
-            for batch in notebook.tqdm(train_data, total=steps_per_epoch):
-                # Perform train step
-                step_result = self.train_step(batch)
 
                 # Collect results
                 d_losses.append(step_result['d_loss'])
