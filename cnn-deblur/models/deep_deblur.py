d7a252f90c9cc09efc08b0d215d06f24ff7bc783
nihil <mattia.orlandi@studio.unibo.it>
2020-08-06 11:13:56 +0200

    First version of multiscale deblur net

--- cnn-deblur/models/deep_deblur.py
+++ cnn-deblur/models/deep_deblur.py
@@ -1,10 +1,9 @@
-from models.conv_net import ConvNet
-from utils.custom_metrics import ssim, psnr
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Layer, Conv2D, Conv2DTranspose, Add, ELU, BatchNormalization, concatenate
 from tensorflow.keras.models import Model
 from tensorflow.keras.losses import logcosh
 from tensorflow.keras.optimizers import Adam
+from utils.custom_metrics import ssim, psnr
 from typing import Tuple, List, Optional
 
 
@@ -34,9 +33,8 @@ def res_block(in_layer: Layer,
     return x
 
 
-class DeepDeblur(ConvNet):
+class DeepDeblur:
     def __init__(self, input_shape: Tuple[int, int, int]):
-        super(DeepDeblur, self).__init__()
         # Coarsest branch
         in_layer3 = Input(shape=(input_shape[0] // 4, input_shape[1] // 4, input_shape[2]),
                           name='in_layer3')
@@ -98,7 +96,7 @@ class DeepDeblur(ConvNet):
         # Final model
         self.model = Model(inputs=[in_layer1, in_layer2, in_layer3], outputs=[out_layer1, out_layer2, out_layer3])
 
-        # Define loss function
+        # Define and set loss function
         K = 3
 
         def multiscale_logcosh(trueY: List[tf.Tensor], predY: List[tf.Tensor]):
@@ -108,6 +106,44 @@ class DeepDeblur(ConvNet):
                 loss += logcosh(trueY[s], predY[s]) / (scale_shape[0] * scale_shape[1] * scale_shape[2])
             return 1./(2. * K) * loss
 
-        self.model.compile(optimizer=Adam(lr=1e-4),
-                           loss=multiscale_logcosh,
-                           metrics=[ssim, psnr, 'mse', 'mae'])
+        self.g_loss = multiscale_logcosh
+
+        # Set optimizer as Adam with lr=1e-4
+        self.g_optimizer = Adam(lr=1e-4)
+
+    @tf.function
+    def train_step(self,
+                   train_batch: Tuple[tf.Tensor, tf.Tensor]):
+        # Determine height and width
+        height = train_batch[0].shape[0]
+        width = train_batch[0].shape[1]
+        # Prepare Gaussian pyramid
+        blurred_batch1 = train_batch[0]
+        sharp_batch1 = train_batch[1]
+        blurred_batch2 = tf.image.resize(train_batch[0], size=(height // 2, width // 2))
+        sharp_batch2 = tf.image.resize(train_batch[1], size=(height // 2, width // 2))
+        blurred_batch3 = tf.image.resize(train_batch[0], size=(height // 4, width // 4))
+        sharp_batch3 = tf.image.resize(train_batch[1], size=(height // 4, width // 4))
+
+        # Train the network
+        with tf.GradientTape() as g_tape:
+            # Calculate generator's loss
+            g_loss = self.g_loss([blurred_batch1, blurred_batch2, blurred_batch3],
+                                 [sharp_batch1, sharp_batch2, sharp_batch3])
+        # Get gradient w.r.t. network's loss and update weights
+        g_grad = g_tape.gradient(g_loss, self.model.trainable_variables)
+        self.g_optimizer.apply_gradients(zip(g_grad, self.model.trainable_variables))
+
+        # Compute metrics
+        ssim_metrics = []
+        psnr_metrics = []
+        ssim_metrics.append(ssim(sharp_batch1, tf.cast(blurred_batch1, dtype='bfloat16')))
+        psnr_metrics.append(psnr(sharp_batch1, tf.cast(blurred_batch1, dtype='bfloat16')))
+        ssim_metrics.append(ssim(sharp_batch2, tf.cast(blurred_batch2, dtype='bfloat16')))
+        psnr_metrics.append(psnr(sharp_batch2, tf.cast(blurred_batch2, dtype='bfloat16')))
+        ssim_metrics.append(ssim(sharp_batch3, tf.cast(blurred_batch3, dtype='bfloat16')))
+        psnr_metrics.append(psnr(sharp_batch3, tf.cast(blurred_batch3, dtype='bfloat16')))
+
+        return {"g_loss": g_loss,
+                "ssim": tf.reduce_mean(ssim_metrics),
+                "psnr": tf.reduce_mean(psnr_metrics)}
